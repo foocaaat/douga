3b1c94a564c8ed159322356e7a639af868abc6ff
foocaaat <68251294+foocaaat@users.noreply.github.com>
2023-09-01 12:55:50 +0300

    Update douga.sh

--- douga.sh
+++ douga.sh
@@ -1,40 +1,107 @@
 #!/usr/bin/bash
 path="$HOME/douga"
+current_date=$(cat $path/douga | head -1)
+if [ "$1" == "morning" ]; then
 
+if [ ! -f $path/douga ]; then
+    date '+%Y-%m-%d' > $path/douga
+else
+    if [ "$(cat $path/douga | head -1)" == "" ];then
+        date '+%Y-%m-%d' -d "$(cat $path/douga | head -1)" > $path/douga
+    else
+        todate=$(date -d $(date '+%Y-%m-%d') +%s)
+        cond=$(date -d $(cat $path/douga | head -1) +%s )
 
-if [ -n "$1" ]; then
-if [ "$1" != "rev" ]; then
+        if [ $todate -gt $cond ];
+        then
+            sed -i.bak "1 s/.*/$(date '+%Y-%m-%d' -d "$(cat $path/douga | head -1) + 1 days")/" $HOME/douga/douga
+
+        else
+            echo Yu have travel to the end of time 
+        fi  
+
+    fi
+fi
+
+    exit
+fi
+if [ "$1" == "--clean0000" ]; then
+  while IFS= read -r line; do
+            id="$(echo "$line" | awk '{print $2}' )"
+            if [ ! -f "$path/$id" ]; then
+                echo $path/$id
+                sed -i "/$line/d" "$path/douga"
+            fi
+  done < "$path/douga"
+  exit
+fi
+
+if [ "$1" == "-fc" ]; then
+        echo feeeee
+        if [ -f "$2" ]; then
+            if [ ! -f $path/$(basename "$2"| replace ' '  '-') ]; then
+                    echo copying
+                    cp "$2" $path/$(basename "$2"| replace ' '  '-')
+            fi
+            if grep -q "$(basename "$2"| replace ' '  '-')" "$path/douga"; then
+                echo The item is already present in the list.
+            else
+                echo $current_date $(basename "$2"| replace ' '  '-') >> "$path/douga"
+            fi
+            mpv $path/$(basename "$2"| replace ' '  '-') --no-config --no-sub --geometry=15%+1+1
+        elif [ -f "$path/$id" ]; then
+            echo it already exist
+        else
+            echo file where?
+        fi
+        exit
+      # code to handle the -f option
+      # $OPTARG contains the argument to the -f option
+fi
+if [ "$1" == "-fm" ]; then
+        echo feeeee
+        if [ -f "$2" ]; then
+            if [ ! -f $path/$(basename "$2"| replace ' '  '-') ]; then
+                    echo moving
+                    mv "$2" $path/$(basename "$2"| replace ' '  '-')
+            fi
+            if grep -q "$(basename "$2"| replace ' '  '-')" "$path/douga"; then
+                echo The item is already present in the list.
+            else
+                echo $current_date $(basename "$2"| replace ' '  '-') >> "$path/douga"
+            fi
+            mpv --no-sub --geometry=15%+1+1 $path/$(basename "$2"| replace ' '  '-') --no-config
+        elif [ -f "$path/$id" ]; then
+            echo it already exist
+        else
+            echo file where?
+        fi
+        exit
+      # code to handle the -f option
+      # $OPTARG contains the argument to the -f option
+fi
+
+if [ "$1" == "-y" ]; then
   if [ ! -f "$path/douga" ]; then
-    echo "Creating $1 file..."
+    echo "Creating $2 file..."
     touch "$path/douga"
   fi
 
-  current_date=$(cat $HOME/.cache/morning | awk '{print $1;}')
-  if [[ $1 =~ ^https://www.youtube.com/shorts/([a-zA-Z0-9_-]+)$ ]]; then
-    pos=$(expr "$1" : '.*rts/')
-    id=$(echo "$1" | cut -c $((pos+1))-$((pos+11)))
+  if [[ $2 =~ ^https://www.youtube.com/shorts/([a-zA-Z0-9_-]+)$ ]]; then
+    pos=$(expr "$2" : '.*rts/')
+    id=$(echo "$2" | cut -c $((pos+1))-$((pos+11)))
 else
-    pos=$(expr "$1" : '.*h?v=')
-    id=$(echo "$1" | cut -c $((pos+1))-$((pos+11)))
+    pos=$(expr "$2" : '.*h?v=')
+    id=$(echo "$2" | cut -c $((pos+1))-$((pos+11)))
   fi
-  if [ -f "$1" ]; then
-      mpv $1
-      echo $current_date $(basename $1) >> "$path/douga"
-      exit
-  elif [ -f "$path/$id" ]; then
-      echo it already exist
-      exit
-  else
     echo $id
   while [ ! -f "$path/$id" ] ; do
   yt-dlp --path $path -f b -o "%(id)s" -- $id
   done
-  fi
   echo $current_date $id >> "$path/douga"
   echo "Added '$id' to $id file."
   exit
 exit
-fi
 fi
 
 print_today_lines() {
@@ -43,24 +110,29 @@ print_today_lines() {
         for n in "${numbers[@]}"; do
           if [ "$n" -eq "$space" ]; then
             id="$(echo "$line" | awk '{print $2}' )"
-            if [ "$1" == "rev" ]; then
-                items="$path/$id $items"
-                items2=$items2,$id
-            else
                 items="$items $path/$id"
-                items2=$id,$items2
-            fi
+                items2=$items2,$id
           fi
         done
   done < "$path/douga"
 
 }
 
-current_date=$(cat $HOME/.cache/morning | awk '{print $1;}')
 numbers=(1 5 13 29 61 125 253 509 1021 2045 4093)
 print_today_lines $1
+words=($items)
+output=()
+
+for (( i=0, j=${#words[@]}-1; i<=j; i++, j-- )); do
+  output+=(${words[i]})
+  if [[ $i != $j ]]; then
+    output+=(${words[j]})
+  fi
+done
+
+echo ${output[@]}
 
 if [[ $items != "" ]]
 then
-mpv --no-sub --geometry=15%+1-60 --loop-playlist $items
+mpv --no-sub --no-config --geometry=15%+1+1 --loop-playlist ${output[@]}
 fi
